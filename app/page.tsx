b025aafaadaf5b0e9706449c1be791799f9128e6
javlav98 <Jrlavin98@gmail.com>
2026-04-14 22:28:18 -0700

    cloned repo to new PC

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-5xl text-red-500">Noiszer Radio</h1>
+      <h1 className="text-5xl text-black-500">Noiszer Radio
+         </h1>
+
     </div>
   );
 }
